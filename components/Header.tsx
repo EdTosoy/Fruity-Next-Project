c6cb1098dbb74e6f0ab5e0fef0fa4bb8866cad3f
EdTosoy <edberto.tosoy@gmail.com>
2021-02-21 15:01:28 +0800

    update MENU

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,6 @@
+import { useContext } from "react";
+import { MenuContext } from "../ContextAPI/openMenu";
+
 export default function Header() {
   const nav = [
     {
@@ -22,8 +25,23 @@ export default function Header() {
       id: "Contact",
     },
   ];
+
+  const { openMenu, setOpenMenu } = useContext(MenuContext);
   return (
     <div className="grid-container sticky top-0 border-b z-50 bg-white">
+      {openMenu && (
+        <div className="md:hidden  col-start-1  col-end-4">
+          <div className="top-16 border-t border-b shadow-md absolute w-full  text-center  p-6 bg-white ">
+            {nav.map(({ name, id }) => (
+              <a href={`#${id}`} key={name}>
+                <nav className="cursor-pointer navigation my-4 hover:text-purple-800">
+                  {name}
+                </nav>
+              </a>
+            ))}
+          </div>
+        </div>
+      )}
       <header className="col-start-2 col-end-3 flex justify-between items-center py-3 ">
         <h1 className="text-3xl font-extrabold text-purple-900 hover:text-purple-800">
           <a href="#">Fruity</a>
@@ -39,7 +57,13 @@ export default function Header() {
             </a>
           ))}
         </div>
-        <div className="md:hidden cursor-pointer">
+        <div
+          className="md:hidden cursor-pointer"
+          onClick={() => {
+            setOpenMenu((prev) => !prev);
+            console.log(openMenu);
+          }}
+        >
           <box-icon name="menu-alt-right" color="purple" size="md"></box-icon>
         </div>
       </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,27 +7,29 @@ import NewProducts from "components/NewProducts";
 import Promo from "components/Promo";
 import Head from "next/head";
 import Footer from "../components/Footer";
-
+import { MenuProvider } from "../ContextAPI/openMenu";
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>Fruity</title>
-        <meta
-          name="description"
-          content="We Beleive That Healthy Eating, Clean Air And, Gental Char Best Start To Genuine Wellbeing "
-        />
-      </Head>
+    <MenuProvider>
+      <div>
+        <Head>
+          <title>Fruity</title>
+          <meta
+            name="description"
+            content="We Beleive That Healthy Eating, Clean Air And, Gental Char Best Start To Genuine Wellbeing "
+          />
+        </Head>
 
-      <Header />
-      <Hero />
-      <NewProducts />
-      <Promo />
-      <AllProducts />
-      <FeaturedProducts />
-      <Contacts />
-      <Footer />
-    </div>
+        <Header />
+        <Hero />
+        <NewProducts />
+        <Promo />
+        <AllProducts />
+        <FeaturedProducts />
+        <Contacts />
+        <Footer />
+      </div>
+    </MenuProvider>
   );
 }
